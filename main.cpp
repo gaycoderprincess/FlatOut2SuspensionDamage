e426a428236d32a4700a891da4f3ddd35208c8b1
💖 Chloe 💖 <gaycoderprincess@proton.me>
2024-12-28 21:20:11 +0100

    use nya-common-fo2

--- main.cpp
+++ main.cpp
@@ -1,51 +1,33 @@
 #include <windows.h>
-#include <fstream>
 #include "nya_commonhooklib.h"
 
-class Suspension {
-public:
-	float fBumpDamp; // +0
-	float fReboundDamp; // +4
-	uint8_t _8[0x24];
-	float fHealth; // +2C
-	uint8_t _30[0x10];
+#include "fo2.h"
+#include "../nya-common-fouc/fo2versioncheck.h"
 
-	void ApplyDamageValues(float bumpDamp, float reboundDamp) {
-		float damageDelta = 1.0 - ((1.0 - fHealth) * 0.9);
-		fBumpDamp = damageDelta * bumpDamp;
-		fReboundDamp = damageDelta * reboundDamp;
+// fo1 code
+void ApplyDamageValues(Suspension* pSuspension, float bumpDamp, float reboundDamp) {
+	float damageDelta = 1.0 - ((1.0 - pSuspension->fHealth) * 0.9);
+	pSuspension->fBumpDamp = damageDelta * bumpDamp;
+	pSuspension->fReboundDamp = damageDelta * reboundDamp;
+}
+
+void ApplySuspensionDamage(Car* car) {
+	ApplyDamageValues(&car->aSuspension[0], car->fSuspensionFrontBumpDamp,car-> fSuspensionFrontReboundDamp);
+	ApplyDamageValues(&car->aSuspension[1], car->fSuspensionFrontBumpDamp,car-> fSuspensionFrontReboundDamp);
+	ApplyDamageValues(&car->aSuspension[2], car->fSuspensionRearBumpDamp, car->fSuspensionRearReboundDamp);
+	ApplyDamageValues(&car->aSuspension[3], car->fSuspensionRearBumpDamp, car->fSuspensionRearReboundDamp);
+}
+
+void __fastcall FixSuspensionDamage(Car* car) {
+	for (int i = 0; i < 4; i++) {
+		car->aSuspension[i].fHealth = 1;
 	}
-};
-
-class Car {
-public:
-	uint8_t _0[0x1890];
-	Suspension aSuspension[4]; // +1890
-	uint8_t _1990[0x3A8];
-	float fSuspensionFrontBumpDamp; // +1D38
-	float fSuspensionFrontReboundDamp; // +1D3C
-	uint8_t _1D40[0x3C];
-	float fSuspensionRearBumpDamp; // +1D7C
-	float fSuspensionRearReboundDamp; // +1D80
-
-	void ApplySuspensionDamage() {
-		aSuspension[0].ApplyDamageValues(fSuspensionFrontBumpDamp, fSuspensionFrontReboundDamp);
-		aSuspension[1].ApplyDamageValues(fSuspensionFrontBumpDamp, fSuspensionFrontReboundDamp);
-		aSuspension[2].ApplyDamageValues(fSuspensionRearBumpDamp, fSuspensionRearReboundDamp);
-		aSuspension[3].ApplyDamageValues(fSuspensionRearBumpDamp, fSuspensionRearReboundDamp);
-	}
-
-	void FixSuspensionDamage() {
-		for (int i = 0; i < 4; i++) {
-			aSuspension[i].fHealth = 1;
-		}
-		ApplySuspensionDamage();
-	}
-};
+	ApplySuspensionDamage(car);
+}
 
 void __stdcall CalculateSuspensionDamage(Car* pCar, uint32_t damageValue, int nTire) {
 	pCar->aSuspension[nTire].fHealth = *(float*)&damageValue;
-	pCar->ApplySuspensionDamage();
+	ApplySuspensionDamage(pCar);
 }
 
 uintptr_t SuspensionDamageASM_jmp = 0x42708A;
@@ -65,10 +47,6 @@ void __attribute__((naked)) SuspensionDamageASM() {
 	);
 }
 
-void __fastcall FixSuspensionDamage(Car* pCar) {
-	pCar->FixSuspensionDamage();
-}
-
 void __attribute__((naked)) FixSuspensionDamageASM() {
 	__asm__ (
 		"pushad\n\t"
@@ -86,11 +64,7 @@ void __attribute__((naked)) FixSuspensionDamageASM() {
 BOOL WINAPI DllMain(HINSTANCE, DWORD fdwReason, LPVOID) {
 	switch( fdwReason ) {
 		case DLL_PROCESS_ATTACH: {
-			if (NyaHookLib::GetEntryPoint() != 0x202638) {
-				MessageBoxA(nullptr, "Unsupported game version! Make sure you're using DRM-free v1.2 (.exe size of 2990080 bytes)", "nya?!~", MB_ICONERROR);
-				exit(0);
-				return TRUE;
-			}
+			DoFlatOutVersionCheck(FO2Version::FO2_1_2);
 
 			NyaHookLib::PatchRelative(NyaHookLib::JMP, 0x427080, &SuspensionDamageASM);
 			NyaHookLib::PatchRelative(NyaHookLib::JMP, 0x4277AB, &FixSuspensionDamageASM);
